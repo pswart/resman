a701db52e18a1bd98e2a212af80424557464e504
paul17843 <paul@ska.ac.za>
2019-11-20 16:14:52 +0200

    Adding package stuff

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='resman',
+      version='0.1',
+      description='Resource manager',
+      url='http://github.com/pswart/resman',
+      author='Paul Swart',
+      author_email='paul.oorkant@gmail.com',
+      license='LGPL-3.0',
+      packages=['resman'],
+      zip_safe=False)
