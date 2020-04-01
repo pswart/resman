5957093e13fbc0239f775e8606b57207dd821148
paul17843 <paul@ska.ac.za>
2020-04-01 22:45:22 +0200

    adding docs

--- setup.py
+++ setup.py
@@ -8,4 +8,11 @@ setup(name='resman',
       author_email='paul.oorkant@gmail.com',
       license='LGPL-3.0',
       packages=['resman'],
-      zip_safe=False)
+      zip_safe=False,
+      setup_requires=[
+          # dependency for `python setup.py test`
+          'pytest-runner',
+          # dependencies for `python setup.py build_sphinx`
+          'sphinx',
+          'recommonmark',
+          ])
